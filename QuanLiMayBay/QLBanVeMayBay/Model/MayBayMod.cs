a9e4dd09949d531ce15c4173b57a3e50dcbaeef1
manhhuy243 <34390735+manhhuy243@users.noreply.github.com>
2018-01-10 16:20:08 +0700

    change by manhhuy

---
+++ QuanLiMayBay/QLBanVeMayBay/Model/MayBayMod.cs
@@ -0,0 +1,13 @@
+using QLBanVeMayBay.Object;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanVeMayBay.Model
+{
+
+}
