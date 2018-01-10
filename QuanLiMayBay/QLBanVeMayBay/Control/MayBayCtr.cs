8b721862e79b13a5118edb8d51c058b506c957a4
manhhuy243 <34390735+manhhuy243@users.noreply.github.com>
2018-01-10 16:14:25 +0700

    change by manhhuy

---
+++ QuanLiMayBay/QLBanVeMayBay/Control/MayBayCtr.cs
@@ -0,0 +1,13 @@
+using QLBanVeMayBay.Model;
+using QLBanVeMayBay.Object;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanVeMayBay.Control
+{
+	
+}
