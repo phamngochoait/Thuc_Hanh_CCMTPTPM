f33b17221f865ff70cf2c9bc1996e93b3f679961
manhhuy243 <34390735+manhhuy243@users.noreply.github.com>
2018-01-11 11:49:47 +0700

    change by manhhuy

--- QuanLiMayBay/QLBanVeMayBay/Model/MayBayMod.cs
+++ QuanLiMayBay/QLBanVeMayBay/Model/MayBayMod.cs
@@ -12,5 +12,29 @@ namespace QLBanVeMayBay.Model
 class MayBayMod
     {
 
+        ConnectToSQL con = new ConnectToSQL();
+        SqlCommand cmd = new SqlCommand();
+
+        public DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = "select * from tb_MayBay";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return dt;
+        }
+
     }
 }
