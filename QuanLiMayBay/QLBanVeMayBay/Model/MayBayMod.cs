d4811d7889ceb98dbced8c18faa182c81df9aa78
manhhuy243 <34390735+manhhuy243@users.noreply.github.com>
2018-01-11 12:31:52 +0700

    change by manhhuy

--- QuanLiMayBay/QLBanVeMayBay/Model/MayBayMod.cs
+++ QuanLiMayBay/QLBanVeMayBay/Model/MayBayMod.cs
@@ -76,6 +76,44 @@ class MayBayMod
             return false;
         }
 
+        public bool UpdData(MayBayObj hhObj)
+        {
+            cmd.CommandText = "Update tb_MayBay set TenMB =  N'" + hhObj.TenMayBay + "', SoLuong = " + hhObj.SoLuong + ", DonGia = " + hhObj.DonGia + " Where MaMB= '" + hhObj.MaMayBay + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+        public bool UpdSL(string mahh, int SL)
+        {
+            cmd.CommandText = "Update tb_MayBay set  SoLuong = " + SL + " Where MaMB= '" + mahh + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
 
     }
 }
