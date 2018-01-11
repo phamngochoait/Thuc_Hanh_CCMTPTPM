727cbaa202d305797a6b60b332923875f5be1a04
ngochoa <phamngochoait@gmail.com>
2018-01-11 13:14:29 +0700

    Add file

---
+++ QuanLiMayBay/QLBanVeMayBay/View/frmNhanVien.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLBanVeMayBay.Control;
+using QLBanVeMayBay.Object;
+
+namespace QLBanVeMayBay.View
+{
+    public partial class frmNhanVien : Form
+    {
+        NhanVienCtrl nvCtr = new NhanVienCtrl();
+        NhanVienObj nvObj = new NhanVienObj();
+        private int flagLuu = 0;
+
+
+        public frmNhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmNhanVien_Load(object sender, EventArgs e)
+        {
+            DataTable dtDS = new System.Data.DataTable();
+            dtDS = nvCtr.GetData();
+            dtgvDS.DataSource = dtDS;
+            binhding();
+            DisEnl(false);
+           
+
+        }
+        private void binhding()
+        {
+            txtMa.DataBindings.Clear();
+            txtMa.DataBindings.Add("Text", dtgvDS.DataSource, "MaNV");
+            txtTen.DataBindings.Clear();
+            txtTen.DataBindings.Add("Text", dtgvDS.DataSource, "TenNV");
+            cmbGioiTinh.DataBindings.Clear();
+            cmbGioiTinh.DataBindings.Add("Text", dtgvDS.DataSource, "GioiTinh");
+            txtDiaChi.DataBindings.Clear();
+            txtDiaChi.DataBindings.Add("Text", dtgvDS.DataSource, "DiaChi");
+            txtSDT.DataBindings.Clear();
+            txtSDT.DataBindings.Add("Text", dtgvDS.DataSource, "SDT");
+            dpNamSinh.DataBindings.Clear();
+            dpNamSinh.DataBindings.Add("Text", dtgvDS.DataSource, "NamSinh");
+        }
+
+        private void dtgvDS_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void DisEnl(bool e)
+        {
+            btnThem.Enabled = !e;
+            btnXoa.Enabled = !e;
+            btnSua.Enabled = !e;
+            btnLuu.Enabled = e;
+            btnHuy.Enabled = e;
+            txtMa.Enabled = e;
+            txtTen.Enabled = e;
+            txtDiaChi.Enabled = e;
+            txtSDT.Enabled = e;
+            cmbGioiTinh.Enabled = e;
+            dpNamSinh.Enabled = e;
+        }
+
+     
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            flagLuu = 0;
+            clearData();
+            DisEnl(true);
+            //loadCMB();
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            flagLuu = 1;
+
+            DisEnl(true);
+            loadCMB();
+
+
+        }
+        private void addData(NhanVienObj nv)
+        {
+            nv.MaNhanVien = txtMa.Text.Trim();
+            nv.DiaChi = txtDiaChi.Text.Trim();
+            nv.DienThoai = txtSDT.Text.Trim();
+            nv.TenNhanVien = txtTen.Text.Trim();
+            nv.NamSinh = dpNamSinh.Text.Trim();
+            nv.SDT = txtSDT.Text.Trim();
+            nv.GioiTinh = cmbGioiTinh.Text.Trim();
+            nv.MatKhau = "";
+
+        }
+       private void loadCMB()
+        {
+            cmbGioiTinh.Items.Clear();
+
+            cmbGioiTinh.Items.Add("Nam");
+            cmbGioiTinh.Items.Add("Nữ");
+            dpNamSinh.Text = DateTime.Now.Date.ToShortDateString();
+        }
+        private void clearData()
+        {
+            txtMa.Text = "";
+            txtTen.Text = "";
+            txtDiaChi.Text = "";
+            txtSDT.Text = "";
+            dpNamSinh.Value = DateTime.Now.Date;
+            loadCMB();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Bạn chắc muốn xóa ? ","Xác nhận",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                if (nvCtr.DelData(txtMa.Text.Trim()))
+                    MessageBox.Show("Xóa thành công !", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("Xóa không thành công!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //else
+            //    return;
+            frmNhanVien_Load(sender, e);
+
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            NhanVienObj nvObj = new NhanVienObj();
+            addData(nvObj);
+            if (flagLuu ==0)
+            {
+                if (nvCtr.AddData(nvObj))
+                    MessageBox.Show("Thêm thành công !", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("Thêm không thành công!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (nvCtr.UpdData(nvObj))
+                    MessageBox.Show("Sửa thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                else
+                    MessageBox.Show("Sửa không thành công!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            frmNhanVien_Load(sender, e);
+            //DisEnl(false);
+
+
+
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Bạn chắc muốn hủy thao tác đang làm ?", "Xác nhận hủy", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            frmNhanVien_Load(sender, e);
+            else
+                return ; 
+            //DisEnl(false);
+        }
+
+        private void txtSDT_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSDT_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
