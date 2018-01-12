911cc859f7a1d4f38d7d488bda9ed9f27455add8
= <thanhdatabc97@gmail.com>
2018-01-12 08:28:27 +0700

    FormDangNhap

--- QuanLiMayBay/QLBanVeMayBay/View/frmDangnhap.cs
+++ QuanLiMayBay/QLBanVeMayBay/View/frmDangnhap.cs
@@ -17,7 +17,37 @@ namespace QLBanVeMayBay.View
         {
             InitializeComponent();
         }
+	 private void frmDangnhap_Load(object sender, EventArgs e)
+        {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("server=WIN-PC\\SQLEXPRESS;database=BanVeMayBay;User=sa;pwd=123");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From Login Where USERNAME='" + textBox1.Text + "' and PASSWORD = '" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString()=="1")
+            {
+                this.Hide();
+                frmMain ss = new frmMain();
+                ss.Show();
+            }
+            else
+            {
+                MessageBox.Show("Hãy kiểm tra lại Username hoặc Password");
+            }
+
+       
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+
+            this.Close();
+        }
        
     }
 }
